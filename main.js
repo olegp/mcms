3c2a56bfaf52cdf18246557946c5c30bd9a23361
Maxime Tricoire <max.tricoire@gmail.com>
2013-12-12 10:38:59 +0100

    fix #17 issue

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var COMMON_NODE = global.process;
 var mcms = require('./lib/mcms.js').mcms;
+var response = require('ringo/jsgi/response');
 
-if (require.main === module) {
-  exports.app = mcms();
-  if (!COMMON_NODE) {
-    require("ringo/httpserver").main(module.id);
-  }
+exports.app = mcms();
+
+if (!COMMON_NODE && require.main == module) {
+  require("ringo/httpserver").main(module.id);
 }
